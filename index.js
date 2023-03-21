05824a7f64bae8638e8645bc341e5bb32557c32d
Rishi Joshi <110519406+tangorishi@users.noreply.github.com>
2023-03-22 01:52:28 +0530

    Update index.js
    Made the register button to work , kindly add the required webpage link
    to redirect to the page to which you want the users to register.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 let menu=document.querySelector('#menu-icon');
 let navbar=document.querySelector('.navbar');
+const registerBtn = document.getElementById('register-btn');
+registerBtn.addEventListener('click', () => {
+  // Replace the URL with the actual registration page URL
+  window.location.href = 'https://example.com/register';
+});
 
 menu.onclick=() =>{
     menu.classList.toggle('bx-x');
